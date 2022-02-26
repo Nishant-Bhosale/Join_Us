9056e9addd56dc407b164654e84d92f4da335f43
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-02-26 11:57:36 +0530

    Connected to mysql

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 const { faker } = require("@faker-js/faker");
+const express = require("express");
+
+const PORT = 5000;
+const app = express();
 const mysql = require("mysql");
 
+const connection = mysql.createConnection({
+	host: "localhost",
+	password: "password",
+	user: "root",
+	database: "join_us",
+	port: 3306,
+});
+
+connection.connect((err) => {
+	if (err) {
+		throw err;
+	} else {
+		console.log("connected");
+	}
+});
+app.listen(PORT, () => {
+	console.log(PORT + " is running");
+});
 console.log(faker.internet.email());
