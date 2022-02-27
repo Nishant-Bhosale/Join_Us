354a86297850d7c2008405a07d971d476e44614a
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-02-27 14:47:25 +0530

    Using bodyparser

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const { faker } = require("@faker-js/faker");
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const PORT = 5000;
 const app = express();
 const mysql = require("mysql");
 
 //Sets view engine to EJS
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection({
 	host: "localhost",
@@ -32,12 +33,19 @@ app.get("/", (req, res) => {
 			console.log(err);
 		}
 		console.log(rows);
-		// res.json({ count: rows[0].cnt });
-		res.render("home.ejs");
+		res.render("home.ejs", { count: rows[0].cnt });
 	});
-	// res.json({ result });
 });
 
+app.post("/register", (req, res) => {
+	const q = "INSERT INTO user(email) VALUES ? ";
+
+	connection.query(q, (err, rows) => {
+		if (err) {
+			console.log(err);
+		}
+	});
+});
 // connection.query(
 // 	"CREATE TABLE random(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255))",
 // 	(err, rows) => {
